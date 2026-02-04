5adbb6e48e3af65575769fc1bc16c26a62706fbe
Clonk <pauliusofficial4@gmail.com>
2026-02-04 18:14:17 +0000

    Add src/components/ScanLines.tsx

---
+++ src/components/ScanLines.tsx
@@ -0,0 +1,11 @@
+const ScanLines = () => {
+  return (
+    <>
+      <div className="scan-lines"></div>
+      <div className="vignette"></div>
+      <div className="noise"></div>
+    </>
+  );
+};
+
+export default ScanLines;
